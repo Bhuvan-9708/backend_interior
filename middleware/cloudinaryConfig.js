8a5873cb02337c14a48a6db871bf8b57c155da8d
saurav204 <159419535+saurav204@users.noreply.github.com>
2024-10-11 10:39:19 +0530

    upload large files

--- middleware/cloudinaryConfig.js
+++ middleware/cloudinaryConfig.js
@@ -9,18 +9,19 @@ const upload = multer({ storage });
 const uploadToCloudinary = (fileBuffer, folder, resourceType = 'auto') => {
 
   return new Promise((resolve, reject) => {
-    const uploadStream = cloudinary.uploader.upload_stream(
+    cloudinary.uploader.upload_large(
+      `data:${resourceType};base64,${fileBuffer.toString('base64')}`, 
       {
         folder: folder,
         public_id: uuidv4(),
-        resource_type: resourceType
+        resource_type: resourceType,
+        chunk_size: 6 * 1024 * 1024
       },
       (error, result) => {
         if (error) return reject(error);
         resolve(result);
       }
     );
-    uploadStream.end(fileBuffer);
   });
 };
 
