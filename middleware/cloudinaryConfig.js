f89ac50cbe6442814488797c7d6efe5388839ed1
saurav204 <159419535+saurav204@users.noreply.github.com>
2024-10-11 11:00:56 +0530

    upload large files

--- middleware/cloudinaryConfig.js
+++ middleware/cloudinaryConfig.js
@@ -2,45 +2,43 @@ require('dotenv').config();
 const multer = require('multer');
 const cloudinary = require('../config/cloudinaryConfig');
 const { v4: uuidv4 } = require('uuid');
+const { Readable } = require('stream');
 
-// Set up multer storage (using memory storage for quick access to file buffers)
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-// Function to upload a single file to Cloudinary using upload_large
 const uploadToCloudinary = (fileBuffer, folder, resourceType = 'image') => {
   return new Promise((resolve, reject) => {
-    cloudinary.uploader.upload_large(
-      `data:${resourceType};base64,${fileBuffer.toString('base64')}`, // Convert buffer to base64
+    const uploadStream = cloudinary.uploader.upload_stream(
       {
         folder: folder,
-        public_id: uuidv4(), // Use a unique ID for each file
+        public_id: uuidv4(),
         resource_type: resourceType,
-        chunk_size: 6 * 1024 * 1024 // Set chunk size to 6MB
       },
       (error, result) => {
         if (error) return reject(error);
-        resolve(result); // Resolve with Cloudinary's result
+        resolve(result); 
       }
     );
+
+    const readableStream = new Readable();
+    readableStream.push(fileBuffer);
+    readableStream.push(null);
+    readableStream.pipe(uploadStream); 
   });
 };
 
 // Function to handle multiple image uploads
 const uploadMultipleImages = async (files, folder) => {
   try {
-    // Map through each file and call uploadToCloudinary for each file buffer
-    const uploadPromises = files.map(file => 
-      uploadToCloudinary(file.buffer, folder, 'image')
-    );
+    const uploadPromises = files.map(file => uploadToCloudinary(file.buffer, folder, 'image'));
 
-    // Wait for all the images to be uploaded concurrently
     const results = await Promise.all(uploadPromises);
-    return results; // Return an array of Cloudinary results
+    return results; 
   } catch (error) {
     console.error('Error uploading multiple images:', error);
     throw error;
   }
 };
 
-module.exports = { upload, uploadMultipleImages };
+module.exports = { upload, uploadToCloudinary, uploadMultipleImages };
